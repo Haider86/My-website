072985647dbdc0b7885e495bbc56a710b66fc578
Haider Alsalih <alsalihns@gmail.com>
2018-10-07 21:48:44 +0200

    try to fix issue of php bug

--- mail.php
+++ mail.php
@@ -1,47 +1,77 @@
 <?php
 
 //Get the form fields, remove html tags and whitespace.
-$name = strip_tags(trim($_POST["name"]));
-$name = str_replace(array("\r","\n"),array("",""),$name);
-$email = filter_var(trim($_POST["email"]),FILTER_SANITIZE_EMAIL);
-$message = trim($_POST["message"]);
+//$name = strip_tags(trim($_POST["name"]));
+//$name = str_replace(array("\r","\n"),array("",""),$name);
+//$email = filter_var(trim($_POST["email"]),FILTER_SANITIZE_EMAIL);
+//$message = trim($_POST["message"]);
+//
+//
+////Check the data.
+//
+//if(empty($name)OR empty($message)OR !filter_var($email,FILTER_VALIDATE_EMAIL)){
+//    header("Location:http://alsalihns.se/index.php?success=-1#form");
+//    exit;
+//}
+//
+////Set the recipient email address.
+//$recipient = "alsalihns@gmail.com";
+//
+//
+////Set the email subject.
+//
+//$subject = "New contact form $name";
+//
+////Bulid the email content. 
+//$email_content = "Name: $name\n";
+//$email_content .= "Email: $email\n\n";
+//$email_content .= "Message: \n$message\n";
+//
+//
+////Bulid the email headers.
+//$email_headers = "Form: $name <$email>";
+//
+////Send the email.
+//mail($recipient, $subject, $email_content, $email_headers);
+//    
+////Redirect to index.html page with seccess code 
+//
+//header("Location:http://alsalihns.se/index.php?success=1#form");
+//
+//
 
 
-//Check the data.
+ // Get the form fields, removes html tags and whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-if(empty($name)OR empty($message)OR !filter_var($email,FILTER_VALIDATE_EMAIL)){
-    header("Location:http://alsalihns.se/index.php?success=-1#form");
-    exit;
-}
+    // Check the data.
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: http://www.alsalihns.se/index.php?success=-1#form");
+        exit;
+    }
 
-//Set the recipient email address.
-$recipient = "alsalihns@gmail.com";
+    // Set the recipient email address. Update this to YOUR desired email address.
+    $recipient = "<YOUR EMAIL HERE>";
 
+    // Set the email subject.
+    $subject = "New contact from $name";
 
-//Set the email subject.
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
 
-$subject = "New contact form $name";
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
 
-//Bulid the email content. 
-$email_content = "Name: $name\n";
-$email_content .= "Email: $email\n\n";
-$email_content .= "Message: \n$message\n";
-
-
-//Bulid the email headers.
-$email_headers = "Form: $name <$email>";
-
-//Send the email.
-mail($recipient, $subject, $email_content, $email_headers);
+    // Send the email.
+    mail($recipient, $subject, $email_content, $email_headers);
     
-//Redirect to index.html page with seccess code 
-
-header("Location:http://alsalihns.se/index.php?success=1#form");
-
-
-
-
-
+    // Redirect to the index.html page with success code
+    header("Location: http://www.alsalihn.se/index.php?success=1#form");
 
 
 
